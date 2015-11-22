f1a1fe7fbb803f7f2c9fdd07c5f49b6f0083c634
ScruffR <ScruffR@users.noreply.github.com>
2015-11-22 18:40:38 +0100

    Update KeyPadLCD_Menu.ino

--- firmware/examples/KeyPadLCD_Menu.ino
+++ firmware/examples/KeyPadLCD_Menu.ino
@@ -26,18 +26,9 @@
  http://www.dfrobot.com/wiki/index.php/LCD_KeyPad_Shield_For_Arduino_SKU:_DFR0009
 */
 
-#include "Arduino_KeyPadLCD_Shield/LiquidCrystal.h"
+#include "Arduino_KeyPadLCD_Shield/Arduino_KeyPadLCD_Shield.h"
 
 #undef KEYPAD_PIN_CONFIRMED_3V3  // change to #define once max voltage on A0 confirmed <= 3.3V
-
-enum struct BUTTONS {
-    NONE = 0,
-    SELECT,
-    LEFT,
-    DOWN,
-    UP,
-    RIGHT
-};
 
 #define MENUITEMS 4
 char menu[] = 
